c16a1efcd2586af94d6e6e998c3af7366de15a32
BlackBeans <adrien.mathieu.net@gmail.com>
2020-04-23 11:10:57 +0200

    [main] Improved code readability, scalability and changed user input interface

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 
+# Board -- virtual boards
+
+###############
+### IMPORTS ###
+###############
+
 import sys, os, argparse, datetime
 
+
+
+#############
+#### V&A ####
+#############
+
 __version__ = "1.0"
+__author__ = "BlackBeans"
+
+
+
+###########################
+## COMMANDLINE ARGUMENTS ##
+###########################
 
 parser = argparse.ArgumentParser(
     prog='Board',
@@ -25,19 +44,52 @@ parser.add_argument('--canvas-mul', type=int, help='Real canvas size respect to 
 parser.add_argument('-v', '--version', action='version', version='%(prog)s '+__version__)
 args = parser.parse_args()
 
+
+
+###############
+### IMPORTS ###
+###############
+
 import pygame
+import pygame.gfxdraw
 from pygame.locals import *
 
+
+
+###############
+## CONSTANTS ##
+###############
+
 SCREENSIZE = (args.width,args.height)
-PENWIDTH = args.penwidth
+
+# Save info
+# ********
 SESSION = datetime.datetime.now().strftime(args.session)
 FORMAT = args.format
 DIR = args.dir
 
+# Colors
+# *****
+white = 255,255,255
+grey = 127,127,127
+black = 0,0,0
+
+penwidth = args.penwidth
+
+
+
+#################
+### FUNCTIONS ###
+#################
+
+# Quit
+# ****
 def quit(exitcode=0):
     pygame.quit()
     sys.exit(exitcode)
 
+# Tuples
+# ******
 def mul_tuple(r, t):
     return tuple(r*v for v in t)
     
@@ -53,105 +105,153 @@ def realpos(pos):
 def relpos(pos):
     return add_tuples(pos, offset)
 
+# Save to page
+# ************
 def save():
     global page
     page += 1
     pygame.image.save(screen, os.path.join(DIR, ("%s-%s.%s" % (SESSION, page, FORMAT))))
 
+
+
+#############
+### SETUP ###
+#############
+
+# Session dir
+# ***********
 if not os.path.isdir(DIR):
     os.makedirs(DIR)
 
+# Pygame
+# ******
 pygame.init()
 pygame.mixer.quit()
+
 screen = pygame.display.set_mode(SCREENSIZE)
 
 pygame.display.set_caption('Tableau')
 
-white = 255,255,255
-grey = 127,127,127
-black = 0,0,0
-
 surface = pygame.Surface(mul_tuple(args.canvas_mul, SCREENSIZE))
 surface.fill(white)
+
+pygame.mouse.set_cursor(*pygame.cursors.tri_right)
+
+
+
+################
+## INPUT VARS ##
+################
+
+islock = False
 isdown = False
-last = None, None
-
-isrdown = False
-rtopleft = (0,0)
-
-ismdown = False
-mo = (0,0)
+anchor = (None,None)
+lock = None
+# lock may be either:
+#  - 'm1':  draw
+#  - 'm3':  erase
+#  - 'm2':  move
+#  - 'c' :  change pen width
+#  - None:  nothing
 offset = (0,0)
+anchw = penwidth
 
 page = 0
 
-iscdown = False
-anchor = 0
-anchw = PENWIDTH
+
+
+###############
+## MAIN LOOP ##
+###############
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: quit()
         #elif event.type == VIDEORESIZE:
         #    screen = pygame.display.set_mode((event.w, event.h), RESIZABLE)
-        elif event.type == MOUSEBUTTONDOWN and event.button == 1:
-            if not iscdown:
-                isdown = True
-                last = realpos(event.pos)
-        elif event.type == MOUSEBUTTONDOWN and event.button == 3:
-            isrdown = True
-            rtopleft = realpos(event.pos)
-        elif event.type == MOUSEBUTTONUP and event.button == 3:
-            isrdown = False
-            x1, y1 = rtopleft
-            x2, y2 = realpos(event.pos)
-            left = min(x1,x2)
-            w = max(x1,x2) - left
-            top = min(y1,y2)
-            h = max(y1,y2) - top
-            pygame.draw.rect(surface, white, pygame.Rect(left, top, w, h))
-        elif event.type == MOUSEBUTTONDOWN and event.button == 2:
-            ismdown = True
-            mo = event.pos
-        elif event.type == MOUSEBUTTONUP and event.button == 2:
-            ismdown = False
-        elif event.type == MOUSEMOTION:
-            if isdown:
-                if last == (None,None):
-                    last = realpos(event.pos)
-                pygame.draw.line(surface,black,last,realpos(event.pos),PENWIDTH)
-                last = realpos(event.pos)
-            elif last != (None, None):
-                last = (None, None)
-            if ismdown:
-                d = sub_tuples(pygame.mouse.get_pos(), mo)
-                offset = add_tuples(offset, d)
-                mo = add_tuples(mo, d)
-            if iscdown:
-                d = (pygame.mouse.get_pos()[0] - anchor)//10
-                PENWIDTH = max(anchw+d,1)
         elif event.type == MOUSEBUTTONUP and event.button == 1:
             isdown = False
-            last = (None, None)
+            if lock == 'm1':
+                islock = False
+                lock = None
+            elif lock == 'm3':
+                x1, y1 = anchor
+                x2, y2 = realpos(event.pos)
+                left = min(x1,x2)
+                w = max(x1,x2) - left
+                top = min(y1,y2)
+                h = max(y1,y2) - top
+                pygame.draw.rect(surface, white, pygame.Rect(left, top, w, h))
+            elif lock == 'm2':
+                pass
+            elif lock == 'c':
+                anchw = penwidth
+            anchor = (None,None)
+        elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+            isdown = True
+            anchor = realpos(event.pos)
+            if not islock:
+                islock = True
+                lock = 'm1'
+            elif lock == 'c':
+                anchw = penwidth
+        elif event.type == MOUSEBUTTONDOWN and event.button == 3:
+            if not islock:
+                islock = True
+                lock = 'm3'
+        elif event.type == MOUSEBUTTONDOWN and event.button == 2:
+            if not islock:
+                islock = True
+                lock = 'm2'
+        elif event.type == MOUSEBUTTONUP and event.button == 3:
+            if lock == 'm3':
+                islock = False
+                lock = None
+        elif event.type == MOUSEBUTTONUP and event.button == 2:
+            if lock == 'm2':
+                islock = False
+                lock = None
+        elif event.type == MOUSEMOTION:
+            if not islock:
+                pass
+            if not isdown:
+                pass
+            if lock == 'm1' and isdown:
+                if anchor == (None,None):
+                    last = realpos(event.pos)
+                else:
+                    last = anchor
+                pygame.draw.line(surface,black,last,realpos(event.pos),penwidth)
+                anchor = realpos(event.pos)
+            elif lock == 'm2' and isdown:
+                d = sub_tuples(realpos(pygame.mouse.get_pos()), anchor)
+                offset = add_tuples(offset, d)
+                mo = add_tuples(anchor, d)
+            elif lock == 'c' and isdown:
+                d = (realpos(pygame.mouse.get_pos())[0] - anchor[0])//10
+                penwidth = max(anchw+d,1)
         elif event.type == KEYDOWN:
             if event.key == ord('s'):
                 save()
             elif event.key == ord('q'):
                 quit()
             elif event.key == ord('c'):
-                iscdown = True
-                anchor = pygame.mouse.get_pos()[0]
-                anchw = PENWIDTH
+                if not islock:
+                    islock = True
+                    lock = 'c'
         elif event.type == KEYUP:
             if event.key == ord('c'):
-                iscdown = False
+                if lock == 'c':
+                    islock = False
+                    lock = None
                 
     screen.fill(white)
     screen.blit(surface, offset)
-    if isrdown:
-        x1, y1 = relpos(rtopleft)
+    if lock == 'm3' and isdown:
+        x1, y1 = relpos(anchor)
         x2, y2 = pygame.mouse.get_pos()
         points = ((x1,y1),(x2,y1),(x2,y2),(x1,y2))
-        pygame.draw.lines(screen, grey, True, points, 4)
-    if iscdown:
-        pygame.draw.circle(screen, grey, pygame.mouse.get_pos(), PENWIDTH)
+        pygame.draw.aalines(screen, grey, True, points, 4)
+    if lock == 'c' and isdown:
+        pygame.draw.circle(screen, grey, pygame.mouse.get_pos(), penwidth)
     pygame.display.flip()
